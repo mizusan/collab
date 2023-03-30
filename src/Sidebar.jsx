6fad9e0473681245a19485d96e39b3335db39e0f
Jaison Joseph <mizulife@gmail.com>
2023-03-30 19:24:19 +0530

    Fix usage of for attribute in React context

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -17,43 +17,43 @@ function Sidebar() {
 			<div className="container mx-auto">
 				<h3>Patient details</h3>
 				<div className="mb-3">
-					<label for="patientFirstName" className="form-label">First Name</label>
+					<label htmlFor="patientFirstName" className="form-label">First Name</label>
 					<input type="text" className="form-control" id="patientFirstName" />
 				</div>
 
 				<div className="mb-3">
-					<label for="patientLastName" className="form-label">Last Name</label>
+					<label htmlFor="patientLastName" className="form-label">Last Name</label>
 					<input type="text" className="form-control" id="patientLastName" />
 				</div>
 
 				<div className="mb-3">
-					<label for="patientDOB" className="form-label">Date of birth:</label>
+					<label htmlFor="patientDOB" className="form-label">Date of birth:</label>
 					<input type="date" className="form-control" id="patientDOB" />
 				</div>
 
 				<div className="row">
 					<div className="col">
 						<div className="mb-3">
-							<label for="patientHeight" className="form-label">Height (cm.)</label>
+							<label htmlFor="patientHeight" className="form-label">Height (cm.)</label>
 							<input type="number" className="form-control" id="patientHeight" />
 						</div>
 					</div>
 
 					<div className="col">
 						<div className="mb-3">
-							<label for="patientWeight" className="form-label">Weight (Kg.)</label>
+							<label htmlFor="patientWeight" className="form-label">Weight (Kg.)</label>
 							<input type="number" className="form-control" id="patientWeight" />
 						</div>
 					</div>
 				</div>
 
 				<div className="mb-3">
-					<label for="patientPhone" className="form-label">Phone</label>
+					<label htmlFor="patientPhone" className="form-label">Phone</label>
 					<input type="phone" className="form-control" id="patientPhone" />
 				</div>
 
 				<div className="mb-3">
-					<label for="exampleInputEmail1" className="form-label">Email address</label>
+					<label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
 					<input type="email" className="form-control" id="exampleInputEmail1" />
 					<div id="emailHelp" className="form-text">We'll never share your personal details with anyone else.</div>
 				</div>
